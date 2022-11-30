dd4ea55c5455f66c4ffbaabe45135f3e2998ef2e
danmao124 <dan018982@gmail.com>
2022-11-30 13:52:10 -0800

    save

--- scraper/scraper2.py
+++ scraper/scraper2.py
@@ -2,3 +2,9 @@ from requests_html import HTMLSession
 
 session = HTMLSession()
 r = session.get('https://www.reddit.com/posts/2019/january-1-1/')
+posts = r.html.find('.DirectoryPost')
+
+for post in posts:
+    print(post.find('.DirectoryPost__Title')[0].text)
+    print(post.find('.DirectoryPost__Subreddit')[0].text)
+    print(post.find('.DirectoryPost__Stats')[0].text)
