e57600fcc83e59f86563e04b620e6d440c9fb213
danmao124 <dan018982@gmail.com>
2022-11-30 14:38:12 -0800

    uploading 2019

--- scraper/scraper2.py
+++ scraper/scraper2.py
@@ -1,30 +1,46 @@
 from requests_html import HTMLSession
-import re
 import pandas as pd
+import calendar
+from datetime import date, timedelta
+import os
 
-session = HTMLSession()
-r = session.get('https://www.reddit.com/posts/2019/january-1-1/')
-posts = r.html.find('.DirectoryPost')
+start_date = date(2019, 1, 1)
+end_date = date(2020, 1, 1)
+delta = timedelta(days=1)
+while start_date < end_date:
+    year = start_date.year
+    day = start_date.day
+    month = calendar.month_name[start_date.month]
+    start_date += delta
 
-posts_dict = {"Title": [], 
-              "Title Link": [],
-              "Score": [],
-              "Num Comments": [], 
-              "Subreddit": []
-              }
+    session = HTMLSession()
+    r = session.get(f'https://www.reddit.com/posts/{year}/{month}-{day}-1/')
+    print(f'Scraping https://www.reddit.com/posts/{year}/{month}-{day}-1/')
+    
+    posts_dict = {"Title": [], 
+                "Title Link": [],
+                "Score": [],
+                "Num Comments": [], 
+                "Subreddit": []
+                }
+    posts = r.html.find('.DirectoryPost')
+    for post in posts:
+        res = post.find('.DirectoryPost__Stats')[0].text.split(" · ")
 
-for post in posts:
-    res = post.find('.DirectoryPost__Stats')[0].text.split(" · ")
+        posts_dict["Title"].append(post.find('.DirectoryPost__Title')[0].text)
+        posts_dict["Title Link"].append(list(post.find('.DirectoryPost__Title')[0].absolute_links)[0])
+        posts_dict["Subreddit"].append(post.find('.DirectoryPost__Subreddit')[0].text)
+        posts_dict["Score"].append(res[0].split(" ")[0])
+        posts_dict["Num Comments"].append(res[1].split(" ")[0])
 
-    posts_dict["Title"].append(post.find('.DirectoryPost__Title')[0].text)
-    posts_dict["Title Link"].append(list(post.find('.DirectoryPost__Title')[0].absolute_links)[0])
-    posts_dict["Subreddit"].append(post.find('.DirectoryPost__Subreddit')[0].text)
-    posts_dict["Score"].append(res[0].split(" ")[0])
-    posts_dict["Num Comments"].append(res[1].split(" ")[0])
+    posts_df = pd.DataFrame(posts_dict)
+    posts_df["Score"] = posts_df["Score"].replace({'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
+    posts_df["Num Comments"] = posts_df["Num Comments"].replace({'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
 
-posts_df = pd.DataFrame(posts_dict)
-posts_df["Score"] = posts_df["Score"].replace({'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
-posts_df["Num Comments"] = posts_df["Num Comments"].replace({'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
+    print(posts_df)
 
-print(posts_df)
-posts_df.to_csv("pep.csv", index=False)
+    if not os.path.exists(f'{year}'):
+        os.makedirs(f'{year}')
+
+    posts_df.to_csv(f'{year}/{month}-{day}.csv', index=False)
+
