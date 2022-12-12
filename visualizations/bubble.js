4a72863f4bbb2d44b8286d70e3551ffe4f72578a
Ali Rahman <alirahman1117@gmail.com>
2022-12-12 18:33:20 -0500

    Updated colors and added selector
    Selector includes months and years to show all possible combinations

--- visualizations/bubble.js
+++ visualizations/bubble.js
@@ -5,19 +5,71 @@ const file4 = 'https://raw.githubusercontent.com/danmao124/pruv/main/processed_d
 const file5 = 'https://raw.githubusercontent.com/danmao124/pruv/main/processed_data/2014/December.json';
 const file6 = 'https://raw.githubusercontent.com/danmao124/pruv/main/processed_data/2015/December.json';
 const width = window.innerWidth;
-const height = window.innerHeight;
-var colors = d3.scaleOrdinal(d3.schemeCategory10);
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+const height = window.innerHeight - 300;
+var colors = d3.scaleOrdinal(["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"]);
+var svg = d3.select('#bubble-chart')
+    .style('width', width)
+    .style('height', height);
 var pack = d3.pack()
     .size([width, height])
     .padding(1.5);
 var format = d3.format(",d");
+var month = "January";
+var year = "2010";
+var year2 = "2010";
+var allGroup = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var selector = d3.select("#selectButton")
+      .selectAll('myOptions')
+      .data(allGroup)
+      .enter()
+      .append('option')
+      .text(function (d) { return d; }) // text showed in the menu
+      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+// Time
+  var dataTime = d3.range(0, 10).map(function(d) {
+    return new Date(2010 + d, 10, 3);
+  });
+
+  var sliderTime = d3
+  .sliderBottom()
+  .min(d3.min(dataTime))
+  .max(d3.max(dataTime))
+  .step(1000 * 60 * 60 * 24 * 365)
+  .width(300)
+  .tickFormat(d3.timeFormat('%Y'))
+  .tickValues(dataTime)
+  .default(new Date(2010, 10, 3))
+  .on('onchange', val => {
+    d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
+    if(year2 == d3.timeFormat('%Y')(val)){
+
+    } else{
+      year2 = d3.timeFormat('%Y')(val);
+      fetchFile(month, year2);
+    }
+
+  });
+
+var gTime = d3
+  .select('div#slider-time')
+  .append('svg')
+  .attr('width', 500)
+  .attr('height', 100)
+  .append('g')
+  .attr('transform', 'translate(30,30)');
+
+gTime.call(sliderTime);
+
+d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
+
 function redraw(classes){
 
   // transition
   var t = d3.transition()
       .duration(750);
-  console.log(classes)
+  //console.log(classes)
   // hierarchy
   var h = d3.hierarchy({children: classes})
       .sum(function(d) { return d.probability; })
@@ -60,14 +112,6 @@ function redraw(classes){
     .attr("y", function(d){ return d.y; })
     .attr("dy", "1.2em")
     .text(function(d) {return Math.ceil(d.value * 10000) /10000; })
-  /*text.append("text")
-    .transition(t)
-    .attr("x", function(d){ return d.x; })
-    .attr("y", function(d){ return d.y; })
-    .attr("dy", "1.2em")
-    .text(function(d) {return Math.ceil(d.value * 10000) /10000; })*/
-
-
 
   //ENTER
   circle.enter().append("circle")
@@ -147,10 +191,26 @@ var generateChart = data => {
         .style('opacity', 1)
 };
 
+async function fetchFile(month, year){
+  var filename = 'https://raw.githubusercontent.com/danmao124/pruv/main/processed_data/' + year + '/' + month + '.json';
+  console.log(filename);
+  data = await d3.json(filename).then(data => data);
+  redraw(data.data);
+};
+
+d3.select("#selectButton").on("change", function(d) {
+    var selectedOption = d3.select(this).property("value")
+    //console.log(selectedOption);
+    month = selectedOption;
+    //console.log(month);
+    fetchFile(month, year2);
+});
+
 (async () => {
-    data = await d3.json(file).then(data => data);
-    redraw(data.data);
+
+    //
     //generateChart(data.data);
+    /*
     await new Promise(r => setTimeout(r, 2000));
     data = await d3.json(file2).then(data => data);
     redraw(data.data);
@@ -165,5 +225,5 @@ var generateChart = data => {
     redraw(data.data);
     await new Promise(r => setTimeout(r, 2000));
     data = await d3.json(file6).then(data => data);
-    redraw(data.data);
+    redraw(data.data);*/
 })();
