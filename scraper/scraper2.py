09ebe2fb18302cfceea3f921effb5af0ed7b7b7c
danmao124 <dan018982@gmail.com>
2022-12-11 20:03:28 -0800

    delete old data

--- scraper/scraper2.py
+++ scraper/scraper2.py
@@ -16,26 +16,30 @@ while start_date < end_date:
     session = HTMLSession()
     r = session.get(f'https://www.reddit.com/posts/{year}/{month}-{day}-1/')
     print(f'Scraping https://www.reddit.com/posts/{year}/{month}-{day}-1/')
-    
-    posts_dict = {"Title": [], 
-                "Title Link": [],
-                "Score": [],
-                "Num Comments": [], 
-                "Subreddit": []
-                }
+
+    posts_dict = {"Title": [],
+                  "Title Link": [],
+                  "Score": [],
+                  "Num Comments": [],
+                  "Subreddit": []
+                  }
     posts = r.html.find('.DirectoryPost')
     for post in posts:
         res = post.find('.DirectoryPost__Stats')[0].text.split(" · ")
 
         posts_dict["Title"].append(post.find('.DirectoryPost__Title')[0].text)
-        posts_dict["Title Link"].append(list(post.find('.DirectoryPost__Title')[0].absolute_links)[0])
-        posts_dict["Subreddit"].append(post.find('.DirectoryPost__Subreddit')[0].text)
+        posts_dict["Title Link"].append(
+            list(post.find('.DirectoryPost__Title')[0].absolute_links)[0])
+        posts_dict["Subreddit"].append(
+            post.find('.DirectoryPost__Subreddit')[0].text)
         posts_dict["Score"].append(res[0].split(" ")[0])
         posts_dict["Num Comments"].append(res[1].split(" ")[0])
 
     posts_df = pd.DataFrame(posts_dict)
-    posts_df["Score"] = posts_df["Score"].replace({'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
-    posts_df["Num Comments"] = posts_df["Num Comments"].replace({'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
+    posts_df["Score"] = posts_df["Score"].replace(
+        {'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
+    posts_df["Num Comments"] = posts_df["Num Comments"].replace(
+        {'k': '*1e3', 'm': '*1e6'}, regex=True).map(pd.eval).astype(int)
 
     print(posts_df)
 
@@ -43,4 +47,3 @@ while start_date < end_date:
         os.makedirs(f'{year}')
 
     posts_df.to_csv(f'{year}/{month}-{day}.csv', index=False)
-
